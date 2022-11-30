7815d5ce907bbc99d78e2bc2fbc548b17f4bbca0
Fernandes Alves Fabien <fabien@code42.fr>
2022-11-30 06:42:51 +0100

    Added : Start implementing theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import "./App.css";
 import { Form } from "./components";
+import Lottie from "lottie-react";
+import passwordAnimation from "./assets/lotties/password.json";
 
 function App() {
   return (
     <div className="App">
+      {/* <div className="menu"></div> */}
+      <Lottie
+        animationData={passwordAnimation}
+        loop={true}
+        style={{ height: "150px" }}
+      />
       <h1>Password Generator</h1>
       <Form />
     </div>
